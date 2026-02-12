c129565dc9e9c0c68471cf71fa42028942298a19
cherry pick <limhojin956@naver.com>
2026-02-13 01:03:47 +0900

    feat : LV2 구현

--- src/main/java/com/example/project2/user/controller/UserController.java
+++ src/main/java/com/example/project2/user/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+//확인
 @RestController
 @RequiredArgsConstructor
 public class UserController {
